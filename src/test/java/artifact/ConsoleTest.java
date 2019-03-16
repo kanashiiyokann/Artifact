81efa68a4304a331bb4af9f208b3d135c774e6ba
zhaoyouwen <zhaoyouwen@dgg.net>
2019-03-16 17:45:58 +0800

    12

--- src/test/java/artifact/ConsoleTest.java
+++ src/test/java/artifact/ConsoleTest.java
@@ -2,6 +2,10 @@ package artifact;
 
 
 import artifact.modules.common.util.ExcelUtil;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFPalette;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.*;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,14 +22,33 @@ public class ConsoleTest {
 
 
     public static void test() throws Exception {
-        String path = "C:\\Users\\yayi\\Desktop\\text1.xls";
+        String path = "C:\\Users\\DGG-S27-D-20\\Desktop\\text1.xls";
 
         ExcelUtil excelUtil = new ExcelUtil();
 
-        excelUtil.prepare()
-                .nextSheet()
-                .nextRow()
-                .nextCell("your", "mother", "will", "die", "this", "afternoon")
+        CellStyle style_center = excelUtil.createStyle();
+        style_center.setAlignment(HorizontalAlignment.CENTER);
+
+        CellStyle style_right = excelUtil.createStyle();
+        style_right.setAlignment(HorizontalAlignment.RIGHT);
+
+        CellStyle style_header = excelUtil.createStyle();
+        Short color = excelUtil.getColor(192, 192, 192);
+        // Short color_border = excelUtil.getColor(0, 0, 0);
+
+        style_header.setAlignment(HorizontalAlignment.CENTER);
+        style_header.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        style_header.setFillForegroundColor(color);
+        style_header.setBorderBottom(BorderStyle.THIN);
+        style_header.setBorderTop(BorderStyle.THIN);
+        style_header.setBorderRight(BorderStyle.THIN);
+        style_header.setBorderLeft(BorderStyle.THIN);
+
+        excelUtil.nextSheet(16, 16, 16, 16).nextRow()
+                .nextCell("凭证汇总表", 4, 1, style_center).nextRow()
+                .nextCell("凭证总张数：12张  附件总张数：11张", 4, 1, style_right).nextRow()
+                .nextCell("1102测试账套", 2, 1).nextCell("2018-06-01 至 2018-06-30", 2, 1, style_right).nextRow()
+                .setStyle(style_header).nextCell("科目编码", "科目名称", "借方余额", "贷方余额").clearStyle().nextRow()
                 .fetch(path);
 
     }
