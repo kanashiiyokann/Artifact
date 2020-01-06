5b974dc3f1cbea5a6c46b157ba558d61660a0bce
zhaoyouwen <zhaoyouwen@dgg.net>
2020-01-06 16:53:12 +0800

    链式调用

--- src/main/webapp/static/js/temp.js
+++ src/main/webapp/static/js/temp.js
@@ -1,20 +1,42 @@
 class Test {
 
     constructor(fn) {
-    this.fn=fn;
-    console.log(fn);
+        this._fn = fn;
     }
 
     static of(fn) {
         return new Test(fn);
     }
 
+    catch(fn) {
+        this._reject = fn;
+        return this;
+    }
+
+    then(fn) {
+        this._accept = fn;
+        return this;
+    }
+
+    next(fn) {
+        let next = Test.of(fn);
+        this._accept = function (data) {
+            next._deliver = true;
+            next.execute(data);
+        };
+        this._next = next;
+        next._prev = this;
+
+        return next;
+    }
+
+    execute(data) {
+        let cursor = this;
+        while (!cursor._deliver && cursor._prev) {
+            cursor = cursor._prev;
+        }
+        delete cursor._deliver;
+        cursor._fn.call(undefined, cursor._accept, cursor._reject, data);
+
+    }
 }
-
-
-// let chain=
-//     Test.of((accept, reject,data) => {}).catch()
-//     .next((accept, reject,data) => {}).catch()
-//         .next((accept, reject,data) => {}).catch().then();
-//
-// chain.start();
